fd783d117ef4a31bedf07a820650197776c21c6a
Abhishek sahu <104501813+Abhisheksahu12@users.noreply.github.com>
2022-09-15 10:17:42 +0530

    Add files via upload

--- ques.c
+++ ques.c
@@ -5,9 +5,20 @@
 int main()
 {
 
+/*
 printf("\n The kernel version is: \n");
 system("cat /proc/sys/kernel/osrelease");
 printf("\n The CPU info: \n");
 system("cat /proc/cpuinfo\n");
 return 0;
 }
+*/
+
+int a;
+printf("\nKernel Version:");
+system("cat /proc/sys/kernel/osrelease");
+printf("\nConfigured free and Used memory:\n");
+system("cat /proc/meminfo | awk 'NR==1, NR==2 {print}'");
+return 0;
+}
+

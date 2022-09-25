15f0af973eb94899c06f5a547dc8881be1a3e30a
Abhishek sahu <104501813+Abhisheksahu12@users.noreply.github.com>
2022-09-25 11:36:25 +0530

    Update prac2.c

--- prac2.c
+++ prac2.c
@@ -6,8 +6,8 @@ int main()
 {
 
 printf("\n The kernel version is: \n");
-system("cat/proc/sys/kernel/osrelease");
+system("cat /proc/sys/kernel/osrelease");
 printf("\n The CPU info: \n");
-system("cat/proc/cpuinfo\n");
+system("cat /proc/cpuinfo\n");
 return 0;
 }
